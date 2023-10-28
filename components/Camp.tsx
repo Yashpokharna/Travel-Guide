45217e7ab8a4cddfbe73aa9ca01a36135f14d292
Yash Pokharna <yashpokharna2002@gmail.com>
2023-10-28 12:09:57 +0530

    Features section done

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -50,11 +50,11 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps)
 const Camp = () => {
   return (
     <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
-      <div className="hide-scrollbar cursor-pointer flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite 
           backgroundImage="bg-bg-img-1"
-          title="Spiti Valley Camping"
-          subtitle="Himachal Pradesh"
+          title="Putuk Truno Camp"
+          subtitle="Prigen, Pasuruan"
           peopleJoined="50+ Joined"
         />
         <CampSite 
@@ -86,4 +86,4 @@ const Camp = () => {
   )
 }
 
-export default Camp
+export default Camp;
